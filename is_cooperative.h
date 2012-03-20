481e40bdc254eae1966cb9e27ceaba63b216b156
Bryan Catanzaro <bcatanzaro@acm.org>
2012-03-20 16:40:44 -0700

    Add license boilerplate.

--- is_cooperative.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <thrust/detail/type_traits.h>
-#include <thrust/detail/type_traits/has_nested_type.h>
-
-namespace cooperative {
-__THRUST_DEFINE_HAS_NESTED_TYPE(is_cooperative, cooperative);
-
-}
--- sequences/uniform_sequence.h
+++ sequences/uniform_sequence.h
@@ -1,8 +1,7 @@
 /*
- *   Copyright 2011-2012 NVIDIA Corporation
- *   Copyright 2010-2011 University of California
- *
- *
+ *   Copyright (c) 2012, NVIDIA CORPORATION.  All rights reserved.
+ *   Portions Copyright (c) 2010-2011, University of California.
+ * 
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
  *   You may obtain a copy of the License at
