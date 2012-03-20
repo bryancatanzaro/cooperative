58805ecfc6af3594118857984593a39ea555d0b8
Bryan Catanzaro <bcatanzaro@acm.org>
2012-03-19 18:08:25 -0700

    Transform not functional (synchronizing).

---
+++ cuda/transform.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <thrust/iterator/retag.h>
+#include <cooperative/cuda/tag.h>
+#include <cooperative/is_cooperative.h>
+#include <cooperative/cuda/functional.h>
+
+namespace cooperative {
+namespace cuda {
+
+template<typename InputIterator,
+         typename OutputIterator,
+         typename UnaryFunction>
+// typename thrust::detail::enable_if<is_cooperative<UnaryFunction>::value,
+//                                    OutputIterator>::type
+OutputIterator
+transform(tag,
+          InputIterator first,
+          InputIterator last,
+          OutputIterator result,
+          UnaryFunction op) {
+    std::cout << "My own transform is cooperative!" << std::endl;
+    typedef thrust::tuple<InputIterator, OutputIterator> IteratorTuple;
+    typedef thrust::zip_iterator<IteratorTuple> ZipIterator;
+
+    ZipIterator zipped_result =
+        thrust::for_each(thrust::make_zip_iterator(thrust::make_tuple(first, result)),
+                         thrust::make_zip_iterator(thrust::make_tuple(last, result)),
+                         unary_cooperative_transform_functor<UnaryFunction>(op));
+    return thrust::get<1>(zipped_result);
+                         
+}
+
+// template<typename InputIterator,
+//          typename OutputIterator,
+//          typename UnaryFunction>
+// typename thrust::detail::disable_if<is_cooperative<UnaryFunction>::value,
+//                                     OutputIterator>::type
+// transform(tag,
+//           InputIterator first,
+//           InputIterator last,
+//           OutputIterator result,
+//           UnaryFunction op) {
+//     std::cout << "My own transform is not cooperative" << std::endl;
+//     thrust::transform(thrust::retag<thrust::system::cuda::tag>(first),
+//                       thrust::retag<thrust::system::cuda::tag>(last),
+//                       thrust::retag<thrust::system::cuda::tag>(result),
+//                       op);
+// }
+
+}
+}
